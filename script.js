ab6fdcb25f4fc956f04c76db9ccc3fb168fb8c3e
Fabrice BUREL <burelfabrice35@gmail.com>
2017-11-20 14:53:58 +0100

    index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var schoolApp = angular.module('schoolApp',[]);
+
+schoolApp.controller('studentsCtrl',function($scope){
+  $scope.firstName = 'Émilie';
+  $scope.lastName = 'Gérard';
+  $scope.age = '17';
+  $scope.grade = 'Terminale';
+});
